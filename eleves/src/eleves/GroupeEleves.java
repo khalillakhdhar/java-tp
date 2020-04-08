311c3826661342f63162af97a5f49ca23cda1904
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-04-08 15:13:31 +0200

    Add files via upload

---
+++ eleves/src/eleves/GroupeEleves.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eleves;
+
+/**
+ *
+ * @author pro
+ */
+import java.util.ArrayList;
+
+public class GroupeEleves {
+
+	private ArrayList<Eleve> listeEleves = new ArrayList<Eleve>();
+
+	public GroupeEleves() {
+	}
+
+	public int nombre() {
+		return listeEleves.size();
+	}
+
+	public ArrayList<Eleve> getListe() {
+		return listeEleves;
+	}
+
+	public void ajouterEleve(Eleve eleve) {
+		listeEleves.add(eleve);
+	}
+
+	public Eleve chercher(String nom) {
+		for (Eleve eleve : listeEleves) 
+			if (eleve.getNom().equals(nom)) return eleve;
+		return null;
+	}
+
+	public void lister() {
+		System.out.println("Liste des eleves : ");
+		for (Eleve eleve : listeEleves) {
+			System.out.println(eleve);
+		}
+	}
+}
